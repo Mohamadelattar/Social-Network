3a641c25da780b40c4f83ca07b70afed33b455b2
Mohamadelattar <mohamadelattar99@gmail.com>
2020-04-09 19:11:48 +0100

    Create a Regestrition page

---
+++ register.php
@@ -0,0 +1,25 @@
+<html>
+    
+    <head>
+        <title>Register</title>
+    
+    </head>
+    <body>
+        <form action="register.php" method="POST">
+        <input type="text" name="firstname" placeholder="First Name" required>
+        <br>
+        <input type="text" name="Lastname" placeholder="Last Name" required>
+        <br>
+        <input type="email" name="email" placeholder="Email" required>
+        <br>
+        <input type="password" name="password" placeholder="Password" required>
+        <br>
+        <input type="password" name="confirmPassword" placeholder="Confirm Password" required>
+        <br>
+        <input type="submit" value="Register" name="register">
+        </form>
+        
+    </body>
+
+
+</html>
