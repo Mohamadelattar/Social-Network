9d1a0ffa3a6c923ba93e3df18ddb1544ce1c9847
Mohamadelattar <mohamadelattar99@gmail.com>
2020-04-10 21:32:41 +0100

    Set TimeZone

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+    ob_start(); // Turn on output buffering
+    session_start();
+
+    $timezone = date_default_timezone_set("Africa/Casablanca");
+    $con = mysqli_connect("localhost","root","","social");
+    if(mysqli_connect_errno())
+    {
+        echo "Failed to connect". mysqli_connect_errno();
+    }
+
+
+
+
+
+?>
