2efaa9cf8ee9581503c3fe58d4e95fa296c38236
Mohamadelattar <mohamadelattar99@gmail.com>
2020-04-10 22:54:57 +0100

    Fixing files

--- register.php
+++ register.php
@@ -2,9 +2,73 @@
 
     require 'config/config.php';
     require 'includes/form_handlers/register_handlers.php'
-   
+?>
+ <html>
+      <head>
+         <title>Register</title>
+     
+     </head>
+     <body>
+         <form action="register.php" method="POST">
+         <input type="text" name="firstname" placeholder="First Name" value="<?php 
+             if(isset($_SESSION["firstname"]))
+             {
+                 echo $_SESSION["firstname"];
+             }
+         ?>" required>
+         <br>
+         <?php
+             if(in_array("Your first name muste be between 4 and 20 characters<br>",$error_array))
+             {
+                 echo "Your first name muste be between 4 and 20 characters<br>";
+             }
+          ?>
+         <input type="text" name="Lastname" placeholder="Last Name" value="<?php
+             if(isset($_SESSION["Lastname"]))
+             {
+                 echo $_SESSION["Lastname"];
+             }
+         
+         ?>" required>
+         <br>
+         <?php
+             if(in_array("Your last name muste be between 4 and 20 characters<br>",$error_array))
+             {
+                 echo "Your last name muste be between 4 and 20 characters<br>";
+             }
+         ?>
+         <input type="email" name="email" placeholder="Email"  value="<?php
+             if(isset($_SESSION["email"]))
+             {
+                 echo $_SESSION["email"];
+             }
+         ?>"required>
+         <br>
+         <input type="email" name="confirmEmail" placeholder="Confirm Email" value="<?php
+             if(isset($_SESSION["confirmEmail"]))
+             {
+                 echo $_SESSION["confirmEmail"];
+             }
+         ?>" required>
+         <br>
+         <?php
+             if(in_array("This Email already in use<br>",$error_array))
+             {
+                 echo "This Email already in use<br>";
+             }
+             else if(in_array("Invalid Email<br>",$error_array))
+             {
+                 echo "Invalid Email<br>";
+             }
+             else if(in_array("Email don't match<br>",$error_array))
+             {
+                 echo "Email don't match<br>";
+             }
+?>
 
-        ?>
+
+
+
         <input type="password" name="password" placeholder="Password" value="<?php
             if(isset($_SESSION["password"]))
             {
