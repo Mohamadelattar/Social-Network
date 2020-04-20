c7bedff0fc627b94ff02a9d792d93f2422a61c5f
Mohamadelattar <mohamadelattar99@gmail.com>
2020-04-20 23:05:01 +0000

    setting page

--- header.php
+++ header.php
@@ -99,7 +99,7 @@
 				 echo '<span class="notification_badge" id="unread_requests">' . $num_requests . '</span>';
 				?>
 			</a>
-			<a href="#">
+			<a href="settings.php">
 				<i class="fa fa-cog fa-lg"></i>
 			</a>
 			<a href="includes/handlers/logout.php">
